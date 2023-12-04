c6b104477d7abf06841d91cbb22c467f426eb317
mcooperr <114104519+mcooperr@users.noreply.github.com>
2023-12-04 01:08:33 -0500

    Add files via upload

--- JS/listener.js
+++ JS/listener.js
@@ -1,17 +1,18 @@
 const images = document.querySelectorAll('.booklist img');
 
 images.forEach((image) => {
-  image.addEventListener('click', () => {
-    const description = image.nextElementSibling;
-    description.classList.toggle('show');
-    description.scrollIntoView({ behavior: 'smooth' });
-
-    if (!description.classList.contains('show')) {
+    image.addEventListener('click', () => {
+      const description = image.nextElementSibling;
+      description.classList.toggle('show');
+      if (description.classList.contains('show')) {
+        description.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      } else {
+        description.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
         window.scrollTo({
-          top: 0,
+          top: window.pageYOffset + description.getBoundingClientRect().top,
           behavior: 'smooth',
         });
-    }
+      }
+    });
   });
-});
   
